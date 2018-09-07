7fe6de1143cac72dddfee1c8d0a0c2d9bbd273c6
DESKTOP-V1FUODV\ajay_admin <>
2018-09-06 23:07:15 -0400

    Added to start with repo

---
+++ src/main/java/com/example/demo/controller/MdeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MdeController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "I am running!";
+	}
+}
